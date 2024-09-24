b562883430b4f1009793928dd5532dd48cdc0215
Rtollinchi <rtollinchi23@gmail.com>
2024-09-24 11:04:26 -0400

    add git route to respond with restaurant by id

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
+const { seedRestaurant } = require("../seedData");
 
 //TODO: Create your GET Request Route Below:
 
@@ -9,6 +10,11 @@ app.get("/restaurants", async (req, res) => {
   const allRestaurants = await Restaurant.findAll();
 
   res.json(allRestaurants);
+});
+
+app.get("/restaurants/:id", async (req, res) => {
+  const id = await Restaurant.findByPk(req.params.id);
+  res.json(id);
 });
 
 module.exports = app;
