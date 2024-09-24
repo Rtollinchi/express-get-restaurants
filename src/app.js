f471eac1e9828373c4a06f4c964b5bbc6c4fbaa2
Rtollinchi <rtollinchi23@gmail.com>
2024-09-24 13:23:14 -0400

    add middleware, post and put route

--- src/app.js
+++ src/app.js
@@ -6,15 +6,52 @@ const { seedRestaurant } = require("../seedData");
 
 //TODO: Create your GET Request Route Below:
 
-app.get("/restaurants", async (req, res) => {
-  const allRestaurants = await Restaurant.findAll();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  res.json(allRestaurants);
+app.get("/restaurants", async (req, res, next) => {
+  try {
+    const allRestaurants = await Restaurant.findAll();
+    res.json(allRestaurants);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get("/restaurants/:id", async (req, res) => {
-  const id = await Restaurant.findByPk(req.params.id);
-  res.json(id);
+app.get("/restaurants/:id", async (req, res, next) => {
+  try {
+    const id = await Restaurant.findByPk(req.params.id);
+    res.json(id);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post("/restaurants", async (req, res, next) => {
+  try {
+    const newRestaurant = await Restaurant.create({
+      name: req.body.name,
+      location: req.body.location,
+      cuisine: req.body.cuisine,
+    });
+    res.json(newRestaurant);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.put("/restaurants/:id", async (req, res, next) => {
+  try {
+    const updatedRestaurant = await Restaurant.findByPk(req.params.id);
+    await updatedRestaurant.update({
+      name: req.body.name,
+      location: req.body.location,
+      cuisine: req.body.cuisine,
+    });
+    res.json(updatedRestaurant);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = app;
